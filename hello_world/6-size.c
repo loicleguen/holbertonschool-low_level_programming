6a0241e03c6ba39ad33a64472f41ffc1dfe939f4
loicleguen <loic.leguen@hotmail.fr>
2025-06-16 12:31:30 +0000

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,16 +6,10 @@
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long longintType;
-	long long longlongintType;
-	float floatType;
-
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long int: %zu bytes\n", sizeof(longintType));
-printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of char: %zu byte\n", sizeof(char));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of long int: %zu bytes\n", sizeof(long int));
+	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of float: %zu bytes\n", sizeof(float));
 		return (0);
 }
