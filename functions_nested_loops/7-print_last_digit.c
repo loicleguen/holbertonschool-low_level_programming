0a0044bab7e9ce89bda12e66b8ac49f469779ab8
loicleguen <loic.leguen@hotmail.fr>
2025-06-18 13:16:35 +0000

    task 7 prints the last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: int
+ * Return: Always "y"
+ */
+
+int print_last_digit(int n)
+{
+	int y = n % 10;
+	if (y < 0)
+	{
+		y = -y;
+		_putchar(y + '0');
+	}
+	else
+	_putchar(y + '0');
+	return (y);
+}
