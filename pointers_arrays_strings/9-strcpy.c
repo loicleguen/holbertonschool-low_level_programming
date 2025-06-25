0ecf9212748ad7b201addc775d7f6a0de6013b9b
loicleguen <loic.leguen@hotmail.fr>
2025-06-25 09:45:05 +0000

    task 9 copies the string pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: char destination
+ * @src: char source
+ * Return: Always (dest) result
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int n;
+
+	for (n = 0; *(src + n) != 0; n++)
+	{
+		*(dest + n) = *(src + n);
+	}
+	return (dest);
+}
