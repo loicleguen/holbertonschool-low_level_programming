0f6a645a4b2221893f16d3b768effa0f5835d877
loicleguen <loic.leguen@hotmail.fr>
2025-06-25 07:30:38 +0000

    task 7 prints half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: char
+ * Return: Always (0) result
+ */
+
+void puts_half(char *str)
+{
+	int n, nsur2;
+
+	for (n = 0; str[n] != 0; n++)
+	{
+		;
+	}
+	if (n % 2 == 0)
+	{
+		nsur2 = n / 2;
+		for (; n > nsur2; nsur2++)
+		{
+			_putchar(str[nsur2]);
+		}
+	}
+	else
+	{
+		nsur2 = (n + 1) / 2;
+		for (; n > nsur2; nsur2++)
+		{
+			_putchar(str[nsur2]);
+		}
+	}
+	_putchar('\n');
+}
