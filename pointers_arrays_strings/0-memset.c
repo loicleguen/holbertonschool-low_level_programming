4b6aa51c4c5e71c7bb139123e7ba1f403a206d56
loicleguen <loic.leguen@hotmail.fr>
2025-06-30 11:53:23 +0200

    memset memcpy

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memeory with a constant byte
+ * @s: char pointer
+ * @b: char
+ * @n: int
+ * return: always char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
