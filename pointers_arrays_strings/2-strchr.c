1de1ef97e222bd719835455be43d184a2a454bca
loicleguen <loic.leguen@hotmail.fr>
2025-06-30 15:37:29 +0200

    task 2 strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,19 +4,20 @@
  * _strchr - locates a character in a string
  * @s: string to be searched
  * @c: character to be located
- * Return: pointer to the first occurrence of c in s, or NULL if not found
+ * * Return: pointer to the first occurrence of c in s,
+ * or NULL if c is not found
  */
 
 char *_strchr(char *s, char c)
 {
-int i;
+	int i;
 
-for (i = 0; s[i] >= '\0'; i++)
-{
-if (s[i] == c)
-{
-return (s + i);
-}
-}
-return ('\0');
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
 }
