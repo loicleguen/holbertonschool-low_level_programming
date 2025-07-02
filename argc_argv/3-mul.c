e7482c70200f11a8eab9861b84b2f692bf539a20
loicleguen <loic.leguen@hotmail.fr>
2025-07-02 10:54:47 +0200

    task 3 multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, n, res = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			n = atoi(argv[i]);
+			res = res * n;
+		}
+		printf("%d\n", res);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
