f33b98236bf9c93e5941ac1643b3149ab4577bca
loicleguen <loic.leguen@hotmail.fr>
2025-07-07 14:55:08 +0200

    task 3 alloc grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -30,9 +30,9 @@ int **alloc_grid(int width, int height)
 		{
 			for (j = 0; j < i; j++)
 			{
-                free(grid[j]);
+				free(grid[j]);
 			}
-            free(grid);
+			free(grid);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
