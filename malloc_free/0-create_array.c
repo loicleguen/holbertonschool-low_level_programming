e52b1309eade6a6cf210b71712625d012b805038
loicleguen <loic.leguen@hotmail.fr>
2025-07-07 11:23:56 +0200

    task 0 create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,17 +14,16 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return (0);
+		return (NULL);
 	}
 	array = malloc(size * sizeof(char));
 	if (array == 0)
 	{
-		return (0);
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
 	return (array);
-	free(array);
 }
