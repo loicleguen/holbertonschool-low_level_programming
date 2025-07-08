0fdff371ab09c78e5288c379365443897327dd6d
loicleguen <loic.leguen@hotmail.fr>
2025-07-08 15:43:18 +0200

    task 2 print struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,13 +10,21 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
 		return;
+	}
 	if (d->name == NULL)
+	{
 		printf("Name: (nil)\n");
+	}
 	else if (d->age == '\0')
+	{
 		printf("Age: (nil)\n");
+	}
 	else if (d->owner == NULL)
+	{
 		printf("Owner: (nil)\n");
+	}
 	else
 	{
 		printf("Name: %s\n", d->name);
