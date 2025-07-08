3fda3e5b2c417c915bb2f16565e9684d280df3fb
loicleguen <loic.leguen@hotmail.fr>
2025-07-08 13:27:56 +0200

    header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,10 +2,22 @@
 #define DOG_H
 
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
+
+
+/**
+ * struct dog - a structure to define a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
 
 
 #endif /* DOG_H */
