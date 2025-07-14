1496953a346ef5cfda6a759834cdb93b162ea9e6
loicleguen <loic.leguen@hotmail.fr>
2025-07-14 10:38:28 +0200

    task 0 print a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: the name to print
+ * @f: the function pointer to use for printing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
