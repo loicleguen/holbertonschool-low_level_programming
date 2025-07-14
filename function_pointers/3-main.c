76a7ae2a78e26a8738ec9aa5f82d72f237fdaf97
loicleguen <loic.leguen@hotmail.fr>
2025-07-14 17:05:18 +0200

    main c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for the calculator program.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings representing the arguments.
+ * Return: 0 on success, or an error code on failure.
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
