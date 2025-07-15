1ebd202551dd2adadac41388e6d7ee5f5028a00e
loicleguen <loic.leguen@hotmail.fr>
2025-07-16 01:21:00 +0200

    task 3 print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -18,17 +18,11 @@ void print_all(const char * const format, ...)
 	while (format && format[i])
 	{
 		if (format[i] == 'c')
-		{
 			printf("%s%c", sep, va_arg(args, int));
-		}
 		else if (format[i] == 'i')
-		{
 			printf("%s%d", sep, va_arg(args, int));
-		}
 		else if (format[i] == 'f')
-		{
 			printf("%s%f", sep, va_arg(args, double));
-		}
 		else if (format[i] == 's')
 		{
 			str = va_arg(args, char *);
