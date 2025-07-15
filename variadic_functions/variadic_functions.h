67f64c30da8fdb94e17e05c9371ce88c0d60785d
loicleguen <loic.leguen@hotmail.fr>
2025-07-16 01:42:58 +0200

    header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,17 @@
 #ifndef variadic_functions_H
 #define variadic_functions_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+typedef struct printer
+{
+	char type;
+	void (*print)(va_list args, char *separator);
+} printer_t;
 
 #endif /* variadic_functions_H */
