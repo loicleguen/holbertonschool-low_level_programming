5de8700c2aa318b8f3ceaa9696d290cb56b94b2a
loicleguen <loic.leguen@hotmail.fr>
2025-07-16 01:41:12 +0200

    task 3 print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,40 +2,90 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * print_char - prints a char
+ * @args: va_list
+ * @separator: string separator
  */
 
+void print_char(va_list args, char *separator)
+{
+	printf("%s%c", separator, va_arg(args, int));
+}
+
+
+/**
+ * print_int - prints an integer
+ * @args: va_list
+ * @separator: string separator
+ */
+
+void print_int(va_list args, char *separator)
+{
+	printf("%s%d", separator, va_arg(args, int));
+}
+
+
+/**
+ * print_float - prints a float
+ * @args: va_list
+ * @separator: string separator
+ */
+
+void print_float(va_list args, char *separator)
+{
+	printf("%s%f", separator, va_arg(args, double));
+}
+
+
+/**
+ * print_string - prints a string
+ * @args: va_list
+ * @separator: string separator
+ */
+
+void print_string(va_list args, char *separator)
+{
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(nil)";
+	printf("%s%s", separator, str);
+}
+
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed
+ */
 
 void print_all(const char * const format, ...)
 {
+	printer_t funcs[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string}
+	};
+	unsigned int i = 0, j;
 	va_list args;
-	unsigned int i = 0;
-	char *str, *sep = "";
+	char *sep = "";
 
 	va_start(args, format);
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
-			printf("%s%c", sep, va_arg(args, int));
-		else if (format[i] == 'i')
-			printf("%s%d", sep, va_arg(args, int));
-		else if (format[i] == 'f')
-			printf("%s%f", sep, va_arg(args, double));
-		else if (format[i] == 's')
+		j = 0;
+		while (j < 4)
 		{
-			str = va_arg(args, char *);
-			if (!str)
-			str = "(nil)";
-			printf("%s%s", sep, str);
+			if (format[i] == funcs[j].type)
+			{
+				funcs[j].print(args, sep);
+				sep = ", ";
+				break;
+			}
+			j++;
 		}
-		else
-		{
-			i++;
-			continue;
-		}
-		sep = ", ";
 		i++;
 	}
 	va_end(args);
