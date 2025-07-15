e9898629330fee263f910165fd9c0a09812b64b5
loicleguen <loic.leguen@hotmail.fr>
2025-07-15 09:22:05 +0200

    3 op func

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,6 +1,4 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 
 /**
@@ -52,11 +50,6 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
@@ -71,10 +64,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
