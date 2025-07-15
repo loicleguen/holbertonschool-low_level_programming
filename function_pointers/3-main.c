f2636e5dcb22524ea2d4946527a5926735cc0c55
loicleguen <loic.leguen@hotmail.fr>
2025-07-15 09:21:01 +0200

    3 main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int a, b, result;
 	int (*op_func)(int, int);
 
 	if (argc != 4)
@@ -20,14 +20,20 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 	op_func = get_op_func(argv[2]);
 	if (op_func == NULL)
 	{
 		printf("Error\n");
-		return (99);
+		exit(99);
 	}
-	printf("%d\n", op_func(a, b));
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = op_func(a, b);
+	printf("%d\n", result);
 	return (0);
 }
