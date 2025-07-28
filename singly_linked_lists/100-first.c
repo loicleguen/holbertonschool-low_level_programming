ee8cb8aa0521571900be432da671498d0dbfe487
loicleguen <loic.leguen@hotmail.fr>
2025-07-28 23:18:00 +0200

    add 100first

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@
  * before main when the program starts.
  */
 
-void first(void)__attribute__((constructor))
+void __attribute__((constructor)) first(void)
 {
 		printf("You're beat! and yet, you must allow,\n");
 		printf("I bore my house upon my back!\n");
