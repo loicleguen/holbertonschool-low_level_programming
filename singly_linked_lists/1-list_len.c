59572e3023d32227fa699941147c27c9322e59a8
loicleguen <loic.leguen@hotmail.fr>
2025-07-28 10:26:31 +0200

    Create 1listlen

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the head of the list
+ * Return: number of elements in the list
+ */
+
+size_t list_len(const list_t *h)
