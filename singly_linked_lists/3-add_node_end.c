cbbf2400a9e98fe2ac9023395449d44e9fd9cb2a
loicleguen <loic.leguen@hotmail.fr>
2025-07-28 13:04:11 +0200

    add 3addnodeend

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -8,3 +8,34 @@
  */
 
 list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *current;
+	char *dup_str;
+
+	if (str == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	return (NULL);
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = dup_str;
+	new_node->len = strlen(dup_str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_node;
+	}
+	return (new_node);
+}
