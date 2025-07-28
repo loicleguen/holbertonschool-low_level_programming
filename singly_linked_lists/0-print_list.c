d57001260155e34e081434ec45720afed6684ee4
loicleguen <loic.leguen@hotmail.fr>
2025-07-28 22:42:19 +0200

    fix 0printlist betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,25 +1,25 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
- * Return: the number of nodes in the list
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+		size_t count = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
+		while (h != NULL)
 		{
-			printf("[%u] %s\n", h->len, h->str);
+				if (h->str == NULL)
+						printf("[0] (nil)\n");
+				else
+						printf("[%u] %s\n", h->len, h->str);
+
+				h = h->next;
+				count++;
 		}
-		h = h->next;
-		count++;
-	}
-	return (count);
+
+		return (count);
 }
