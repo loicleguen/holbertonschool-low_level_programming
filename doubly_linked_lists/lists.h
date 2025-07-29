03a0091cca3dea783db3bea94198b950719ce80c
loicleguen <loic.leguen@hotmail.fr>
2025-07-29 09:40:22 +0200

    fix lists.h library

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 /**
@@ -15,9 +16,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
