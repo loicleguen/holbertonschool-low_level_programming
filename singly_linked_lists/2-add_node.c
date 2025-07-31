b933c77acd5eac05f4197176e9b06fe1d729db72
loicleguen <loic.leguen@hotmail.fr>
2025-07-31 09:45:31 +0200

    fix 2addnode

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning of a list
@@ -11,6 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *dup_str;
+	unsigned int len = 0;
 
 	if (str == NULL)
 		return (NULL);
@@ -23,8 +26,10 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+	while (dup_str[len])
+		len++;
 	new_node->str = dup_str;
-	new_node->len = strlen(dup_str);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
