49eb7a45643e48391dcdf59394cf1f9146ee1af8
loicleguen <loic.leguen@hotmail.fr>
2025-08-01 09:13:20 +0200

    add 0hashtablecreate

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,8 +9,11 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table;
 
+	if (size == 0)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
