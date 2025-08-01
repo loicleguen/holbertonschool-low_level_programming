2903fc5a58a0850e0062714b084a0d2e4fabc3d1
loicleguen <loic.leguen@hotmail.fr>
2025-08-01 09:34:31 +0200

    fix 0hashtablecreate commentaire

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
  *
- * Return: A pointer to the newly created hash table, or NULL on failure.
+ * Return: A pointer to the newly created hash table or NULL on failure
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
